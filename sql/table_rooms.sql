dfb956cc3eed51883065ebba2fa4690c790c5821
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2017-11-17 20:24:42 +0100

    Delete table_rooms.sql

--- sql/table_rooms.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE rooms (
-        id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-        name VARCHAR(255)   NOT NULL,
-        last_updated TIMESTAMP,
-        n_users SMALLINT UNSIGNED,
-        PRIMARY KEY (id)
-);
