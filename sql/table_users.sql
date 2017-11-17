0b16f7fdc6b1fc816ba4d61383f186e243c049b4
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2017-11-17 20:24:49 +0100

    Delete table_users.sql

--- sql/table_users.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-        id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
-        username VARCHAR(25) NOT NULL,
-        password VARCHAR(25) NOT NULL,
-        PRIMARY KEY (id)
-);
