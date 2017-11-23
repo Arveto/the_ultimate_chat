e492319a4d1a5a85d4172aa7ca3104988d260945
TERRUSS <31973542+TERRUSS@users.noreply.github.com>
2017-11-23 19:52:41 +0100

    Add files via upload

--- public/js/rooms/rooms.js
+++ public/js/rooms/rooms.js
@@ -1,5 +1,5 @@
 
-
+var i=0;
 var Room = function (num, roomName, entrants) {  //Room constructor
   this.num = num;
   this.roomName = roomName;
@@ -8,7 +8,7 @@ var Room = function (num, roomName, entrants) {  //Room constructor
   this.disp = function () {
     let div = $('<div>').addClass('room').html('<strong>'+this.roomName+'</strong><span class="entrants">'+this.entrants+' entrants</span>').appendTo('#roomsList');
     div.attr('id', this.num);
-    div.css('background-color', getRandomColor() );
+    div.css('background-color', getColor("room") );
 
     $(".room:last").on('click', function () {     //event 'onclick' => send new room to the server
       socket.emit('leaveRoom');
@@ -17,22 +17,37 @@ var Room = function (num, roomName, entrants) {  //Room constructor
       socket.emit('joinRoom', roomId);      //server emits 'usersList' event
     });
   };
+
+  if(i>=4){
+    i++;
+  } else {
+    i=0
+  }
 };
 
-
+var j=0;
 function changeRoom(pseudo) {
   $("div#entrants").empty();
   $("div#messages").empty();
 
   let div = $('<div>').addClass('entrant').html('<strong>'+pseudo+'</strong>').appendTo('#entrantsList');
-  div.css('background-color', getRandomColor() );
+  div.css('background-color', getColor("entrant") );
+
+  if(i>=4){
+    i++;
+  } else {
+    i=0
+  }
 }
 
-function getRandomColor() {
-  let letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
+function getColor(target) {
+  let room = ['ca4e4e', 'c8794e', 'c9994d', 'c9b64d', 'c3c94f'];
+  let entrant = ['a7baff', 'b8b8ff', 'd0c2ff', 'ddbdfe', 'edbaff'];
+
+  if(target == "room"){
+    let color = '#' + room[i];
+  } else {
+    let color = '#' + entrant[]
   }
   return color;
 }
