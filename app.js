ff3d2e399daa6950ff1d8b25a6b7738df2d63c52
Aurélien Delval <32074290+PurplePachyderm@users.noreply.github.com>
2017-11-23 17:53:14 +0100

    Bugged af

--- app.js
+++ app.js
@@ -3,27 +3,71 @@ var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io').listen(server);
-    path = require('path');
+    path = require('path'),
+    bodyParser = require('body-parser');
+    //cookieParser = require('cookie-parser'); TODO Parse cookies
 
-    var mysql = require('mysql');
-    var connection = mysql.createConnection({
-      host     : 'localhost',
-      user     : 'root',
-      password : 'root',
-      database : 'ultimate_chat'
-    });
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database : 'ultimate_chat'
+});
 
-    var serverEvents = require('./server_events.js');
+var serverEvents = require('./server_events.js');
 
-app.use(express.static(path.join(__dirname, 'js')));
+app.use(express.static(path.join(__dirname, 'public')));
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+//app.use(cookieParser());
 
-//      Routes
+    //Variables
+
+    //Routes
 app.get('/', function(req, res){
-    res.sendFile('debug.html', {root: 'C:\\Programmation\\Web\\Ultimate Chat\\'}); //XXX Page for testing purpose only, remove after
+    /*if(res.cookiers == undefined){
+        res.redirect('/signup');
+    }
+    else{*/
+        res.sendFile('index.html', {root: 'C:\\Programmation\\Web\\Ultimate Chat\\'});
+    //}
+
+});
+
+app.get('/signup', function(req, res){
+    res.sendFile('signup.html', {root: 'C:\\Programmation\\Web\\Ultimate Chat\\'});
+});
+
+app.post('/signup', urlencodedParser, function(req, res){
+        var queryString = "INSERT INTO users (`pseudo`, `password`) VALUES (?, ?)";
+        connection.query(queryString, [req.body.password, req.body.username], function(error, result, fields){
+            if (error) throw error;
+        });
+        res.cookie('password', req.body.password).send('Test');
+        res.cookie('username', req.body.username).send('Test');
+        res.redirect('/');
 });
 
 io.sockets.on('connection', function(socket){
-    serverEvents.events(socket, connection);
+
+        //The user needs to see the rooms list
+    var queryString = 'SELECT name, id, n_users FROM rooms';
+    connection.query(queryString, function (error, result, fields) {    //Selects avalaible rooms
+        if (error) throw error;
+        for(var i=0; i<result.length; i++){
+            socket.emit('room', {'roomName': result[i].name, 'num': result[i].id, 'entrants': result[i].n_users});
+        }
+    });
+
+    serverEvents.chatEvents(socket, connection);
+
+    //The user leaves the site
+    socket.on('disconnect', function(){
+        var queryString = "UPDATE users SET room_id = NULL, socket_id = NULL where socket_id = ?";
+        connection.query(queryString, [socket.id], function(error, result, fields){
+            if (error) throw error;
+        });
+    });
 });
 
 server.listen(80);
--- server_events.js
+++ server_events.js
@@ -1,21 +1,13 @@
-exports.events = function(socket, connection){
-    console.log('User connected');
-        //The user is logged in and needs to see the rooms list
-    socket.on('requireRooms',function(){
-        var queryString = 'SELECT name, id, n_users FROM rooms';
-        connection.query(queryString, function (error, result, fields) {    //Selects avalaible rooms
-            if (error) throw error;
-            socket.emit('rooms', result);
-        });
-    });
+exports.chatEvents = function(socket, connection){
+
 
 
         //The user enters a room
     socket.on('joinRoom', function(id){
         /*The users's room is gonna be updated, he's gonna receive a list of connected users and the last 20 messages.
         The room n_user will be incremented. The previoulsy connected users will receive a notice informing them about the new user.*/
-
-        queryString = "UPDATE users SET room_id = ? WHERE socket_id = ?";
+        console.log('joinRoom detected');
+        var queryString = "UPDATE users SET room_id = ? WHERE socket_id = ?";
         connection.query(queryString, [id, socket.id], function(error, result, fields){  //Update user's 'room_id'
             if (error) throw error;
         });
@@ -29,14 +21,18 @@ exports.events = function(socket, connection){
         connection.query(queryString, [id], function(error, result, fields){  //Select last 20 messages
             if (error) throw error;
 
-            var message;
+            var oldResult = result;
+            console.log(result);
             for(var i=0; i<result.length; i++){
-                message = result[i];
-                socket.emit('message', message);   //The new user is receiving last messages.
+                queryString = "SELECT pseudo FROM users WHERE id = ?";
+                console.log(oldResult[i].content);
+                connection.query(queryString, [oldResult[i].sender_id], function(error, result, fields){
+                    socket.emit('message', {'content': oldResult[i].content, 'pseudo': result.pseudo, 'timestamp': oldResult[i].timestamp});   //The new user is receiving last messages.
+                });  //Select last 20 messages
             }
         });
 
-        queryString = "SELECT pseudo, id FROM users WHERE room_id = ?";
+        queryString = "SELECT pseudo FROM users WHERE room_id = ?";
         connection.query(queryString, [id], function(error, result, fields){  //Select users list
             if (error) throw error;
             socket.emit('usersList', result); //The new user is receiving users list.
@@ -60,17 +56,17 @@ exports.events = function(socket, connection){
 
 
         //A new message is sent by the user
-    socket.on('message',function(content, timestamp){
+    socket.on('message',function(content){
         /*The message must be added to the 'messages' table,
         and all connected users (except the sender) will get a 'message' event.*/
+        var timestamp = Date.now();
 
-        queryString = "INSERT INTO messages (`content`, `sender_id`, `room_id`) VALUES (?, (SELECT id FROM users WHERE socket_id = ?), (SELECT room_id FROM users WHERE socket_id = ?) )";
-
-        connection.query(queryString, [content, socket.id, socket.id], function(error, result, fields){   //Add the message to the batabase
+        queryString = "INSERT INTO messages (`content`, `sender_id`, `room_id`, 'timestamp') VALUES (?, (SELECT id FROM users WHERE socket_id = ?), (SELECT room_id FROM users WHERE socket_id = ?), ?)";
+        connection.query(queryString, [content, socket.id, socket.id, timestamp], function(error, result, fields){   //Add the message to the batabase
             if (error) throw error;
         });
-
-        queryString = "SELECT pseudo FROM users WHERE socket_id = ?";   //Send 'message' event to other users
+            //Send 'message' event to other users
+        queryString = "SELECT pseudo FROM users WHERE socket_id = ?";
         connection.query(queryString, [socket.id], function(error, result, fields){   //Get user's pseudo
             if (error) throw error;
             var pseudo = result.pseudo;
@@ -118,16 +114,6 @@ exports.events = function(socket, connection){
         queryString = "UPDATE users SET room_id = NULL WHERE socket_id = ?"; //Setting user's room to NULL
         connection.query(queryString, [socket.id], function(error, result, fields){
             if (error) throw error;
-        });
-    });
-
-    
-    //The user leaves the site
-    socket.on('disconnect', function(){
-        var queryString = "UPDATE users SET room_id = NULL, socket_id = NULL where socket_id = ?";
-        connection.query(queryString, [socket.id], function(error, result, fields){
-            if (error) throw error;
-            console.log("User left");
         });
     });
 }
