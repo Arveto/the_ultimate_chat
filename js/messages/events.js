7256391fe6171c01b7224cb9d634794c03361146
TERRUSS <31973542+TERRUSS@users.noreply.github.com>
2017-12-11 16:32:08 +0100

    Delete events.js

--- js/messages/events.js
+++
@@ -1,9 +0,0 @@
-
-$("textarea").on('keypress', (e) => {     //send messages
-  if ( e.keyCode == 13 ) {  //enter key
-    e.preventDefault();
-    if ($("textarea").val()){
-      Message.send( $("textarea").val() );
-    }
-  }
-});
